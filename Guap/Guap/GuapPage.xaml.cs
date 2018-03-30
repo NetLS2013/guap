1cff5c66e52f90c8e180a042865a76c5bc607a36
igor k <i.klymyuk@netls.com.ua>
2018-03-30 11:38:19 +0300

    init projects

---
+++ Guap/Guap/GuapPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Guap
+{
+    public partial class GuapPage : ContentPage
+    {
+        public GuapPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
