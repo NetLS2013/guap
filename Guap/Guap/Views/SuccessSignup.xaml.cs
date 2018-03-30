5f5cb9f1eb011ac6a7c099ead7050fbd5efb9333
Yurii Havrylko <yurii.havrylko@gmail.com>
2018-03-30 16:53:47 +0300

    develop welcome page with logo

---
+++ Guap/Guap/Views/SuccessSignup.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Guap.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SuccessSignup : ContentPage
+    {
+        public SuccessSignup()
+        {
+            InitializeComponent();
+        }
+    }
+}
