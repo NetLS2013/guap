5f4b13603adf505b84679237ed822774127b774e
Yurii Havrylko <yurii.havrylko@gmail.com>
2018-04-02 17:43:08 +0300

    add packages, develop generate mnemonic

---
+++ Guap/Guap/GlobalSetting.cs
@@ -0,0 +1,35 @@
+﻿namespace Guap
+{
+    using NBitcoin;
+
+    public class GlobalSetting
+    {
+        private class Nested
+        {
+            static Nested()
+            {
+
+            }
+
+            internal static readonly GlobalSetting Instance = new GlobalSetting();
+        }
+
+        private GlobalSetting()
+        {
+            WordCount = WordCount.Twelve;
+            Wordlist = Wordlist.English;
+            EthereumNetwork = "https://ropsten.infura.io/E8XftGiqmaErL2KN5Cp3";
+            WalletPath = "m/44'/60'/0'/0/x";
+        }
+
+        public static GlobalSetting Instance => Nested.Instance;
+
+        public WordCount WordCount { get; }
+
+        public Wordlist Wordlist { get; }
+
+        public string EthereumNetwork { get; }
+
+        public string WalletPath { get; }
+    }
+}
---
+++ Guap/Guap/Service/EthereumService.cs
@@ -0,0 +1,17 @@
+﻿namespace Guap.Service
+{
+    using NBitcoin;
+
+    using Nethereum.HdWallet;
+    using Nethereum.Web3.Accounts;
+
+    public class EthereumService
+    {
+        public static string[] MnenonicPhrasegenerate()
+        {
+            Mnemonic mnemo = new Mnemonic(GlobalSetting.Instance.Wordlist, GlobalSetting.Instance.WordCount);
+
+            return mnemo.Words;
+        }
+    }
+}
