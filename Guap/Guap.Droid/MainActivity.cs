42e2c2d1869a93661d06e2acd06f604bd8cbf2eb
igor k <i.klymyuk@netls.com.ua>
2018-04-16 16:41:40 +0300

    merge branch

--- Guap/Guap.Droid/MainActivity.cs
+++ Guap/Guap.Droid/MainActivity.cs
@@ -1,41 +1,22 @@
 ﻿using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Runtime;
+using Plugin.Permissions;
 
 namespace Guap.Droid
 {
-    using Android;
-    using Android.Support.V4.App;
-    using Android.Support.V4.Content;
-
-    using Plugin.CurrentActivity;
-    using Plugin.Permissions;
-
     [Activity(Label = "Guap.Droid", Icon = "@drawable/icon", Theme = "@style/MyTheme", MainLauncher = true,
         ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
-        //protected override void OnStart()
-        //{
-            
-        //    if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.ReadExternalStorage) != Permission.Granted)
-        //    {
-        //        ActivityCompat.RequestPermissions(this, new String[] { Manifest.Permission.ReadExternalStorage, Manifest.Permission.WriteExternalStorage }, 0);
-        //    }
-        //    else
-        //    {
-        //        System.Diagnostics.Debug.WriteLine("Permission Granted!!!");
-        //    }
-        //    base.OnStart();
-        //}
-
         protected override void OnCreate(Bundle bundle)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
             
             base.OnCreate(bundle);
-          //  CrossCurrentActivity.Current.Activity = this;
+            
             global::Xamarin.Forms.Forms.Init(this, bundle);
             
             App.ScreenHeight = (int) (Resources.DisplayMetrics.HeightPixels / Resources.DisplayMetrics.Density);
@@ -47,6 +28,7 @@ namespace Guap.Droid
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            
             PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
